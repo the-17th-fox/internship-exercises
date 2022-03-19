89478f09b7b167a4aa99ba8849cca18501053396
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-03-19 18:02:27 +0300

    LogginProxy init

---
+++ LoggingProxy/LogginProxy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingProxy
+{
+    public class LogginProxy <T> : DynamicObject
+    {
+        public T CreateInstance(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ LoggingProxy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggingProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
