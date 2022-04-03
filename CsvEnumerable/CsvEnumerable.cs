aa28bdda658a62aa9e01b4bd84d31eac2f5149f8
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-04-03 15:37:38 +0300

    Added CsvEnumerable solution

---
+++ CsvEnumerable/CsvEnumerable.cs
@@ -0,0 +1,37 @@
+﻿using CsvHelper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvEnumerable
+{
+    public class CsvEnumerable<T> : IEnumerable<T>, IDisposable
+    {
+        private bool _disposed = false;
+
+        private CsvReader _csvReader;
+
+        public CsvEnumerable(CsvReader csvReader) => _csvReader = csvReader;
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+
+            GC.Collect();
+            _disposed = true;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in _csvReader.GetRecords<T>())
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ CsvEnumerable/Program.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace CsvEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new StreamReader($"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent}\\text.csv"))
+            using (var records = new CsvEnumerable<Person>(new CsvReader(stream, CultureInfo.InvariantCulture)))
+            {
+                foreach (var item in records)
+                {
+                    Console.WriteLine($"{item.Id}\t{item.Name}");
+                }
+            }
+        }
+    }
+}
